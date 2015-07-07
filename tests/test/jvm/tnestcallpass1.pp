krisfremen/fpc

commit 88dd8511bbcfb1722fd9d22845961cdf419769c5

* record a load node for the self/vmt tree of the current routine in the     tcallnode constructor, so that when it's needed later during pass 1,     its value doesn't depend on the context in which pass 1 is executed     (e.g. when inlining) (mantis #18121)
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@30908 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/ncal.pas
@@ -121,6 +121,12 @@ interface
           procdefinitionderef : tderef;
           { tree that contains the pointer to the object for this method }
           methodpointer  : tnode;
+          { tree that contains the self/vmt parameter when this node was created
+            (so it's still valid when this node is processed in an inline
+             context)
+          }
+          call_self_node,
+          call_vmt_node: tnode;
           { initialize/finalization of temps }
           callinitblock,
           callcleanupblock : tblocknode;
@@ -1348,6 +1354,21 @@ implementation
          funcretnode:=nil;
          paralength:=-1;
          varargsparas:=nil;
+         if assigned(current_structdef) and
+            assigned(mp) then
+           begin
+             { can't determine now yet if it will be necessary or not, so
+               always create it if there is a 'self' symbol in the current
+               context }
+             if get_local_or_para_sym('self')<>nil then
+               call_self_node:=load_self_node;
+            { only needed when calling a destructor from an exception block in a
+              contructor of a TP-style object }
+            if is_object(current_structdef) and
+               (current_procinfo.procdef.proctypeoption=potype_constructor) and
+               (cnf_create_failed in callflags) then
+              call_vmt_node:=load_vmt_pointer_node;
+           end;
       end;
 
 
@@ -1470,6 +1491,8 @@ implementation
       begin
         callinitblock:=tblocknode(ppuloadnode(ppufile));
         methodpointer:=ppuloadnode(ppufile);
+        call_self_node:=ppuloadnode(ppufile);
+        call_vmt_node:=ppuloadnode(ppufile);
         callcleanupblock:=tblocknode(ppuloadnode(ppufile));
         funcretnode:=ppuloadnode(ppufile);
         inherited ppuload(t,ppufile);
@@ -1485,6 +1508,8 @@ implementation
       begin
         ppuwritenode(ppufile,callinitblock);
         ppuwritenode(ppufile,methodpointer);
+        ppuwritenode(ppufile,call_self_node);
+        ppuwritenode(ppufile,call_vmt_node);
         ppuwritenode(ppufile,callcleanupblock);
         ppuwritenode(ppufile,funcretnode);
         inherited ppuwrite(ppufile);
@@ -1501,6 +1526,10 @@ implementation
         procdefinitionderef.build(procdefinition);
         if assigned(methodpointer) then
           methodpointer.buildderefimpl;
+        if assigned(call_self_node) then
+          call_self_node.buildderefimpl;
+        if assigned(call_vmt_node) then
+          call_vmt_node.buildderefimpl;
         if assigned(callinitblock) then
           callinitblock.buildderefimpl;
         if assigned(callcleanupblock) then
@@ -1522,6 +1551,10 @@ implementation
         procdefinition:=tabstractprocdef(procdefinitionderef.resolve);
         if assigned(methodpointer) then
           methodpointer.derefimpl;
+        if assigned(call_self_node) then
+          call_self_node.derefimpl;
+        if assigned(call_vmt_node) then
+          call_vmt_node.derefimpl;
         if assigned(callinitblock) then
           callinitblock.derefimpl;
         if assigned(callcleanupblock) then
@@ -1583,6 +1616,14 @@ implementation
           n.methodpointer:=methodpointer.dogetcopy
         else
           n.methodpointer:=nil;
+        if assigned(call_self_node) then
+          n.call_self_node:=call_self_node.dogetcopy
+        else
+          n.call_self_node:=nil;
+        if assigned(call_vmt_node) then
+          n.call_vmt_node:=call_vmt_node.dogetcopy
+        else
+          n.call_vmt_node:=nil;
         { must be copied before the funcretnode, because the callcleanup block
           may contain a ttempdeletenode that sets the tempinfo of the
           corresponding temp to ti_nextref_set_hookoncopy_nil, and this nextref
@@ -2033,7 +2074,7 @@ implementation
         { inherited }
         else if (cnf_inherited in callnodeflags) then
           begin
-            selftree:=load_self_node;
+            selftree:=call_self_node;
            { we can call an inherited class static/method from a regular method
              -> self node must change from instance pointer to vmt pointer)
            }
@@ -2089,7 +2130,7 @@ implementation
                           end;
                       end
                     else
-                      selftree:=load_self_node
+                      selftree:=call_self_node
                   else
                     selftree:=methodpointer.getcopy;
                 end;
@@ -2126,7 +2167,7 @@ implementation
         else
           begin
             if methodpointer.nodetype=typen then
-              selftree:=load_self_node
+              selftree:=call_self_node
             else
               selftree:=methodpointer.getcopy;
           end;
@@ -2369,7 +2410,7 @@ implementation
              temp:=ctempcreatenode.create(objcsupertype,objcsupertype.size,tt_persistent,false);
              addstatement(statements,temp);
              { initialize objc_super record }
-             selftree:=load_self_node;
+             selftree:=call_self_node;
 
              { we can call an inherited class static/method from a regular method
                -> self node must change from instance pointer to vmt pointer)
@@ -2539,7 +2580,7 @@ implementation
             else
               { destructor called from exception block in constructor }
               if (cnf_create_failed in callnodeflags) then
-                vmttree:=ctypeconvnode.create_internal(load_vmt_pointer_node,voidpointertype)
+                vmttree:=ctypeconvnode.create_internal(call_vmt_node,voidpointertype)
             else
               { inherited call, no create/destroy }
               if (cnf_inherited in callnodeflags) then
@@ -3562,6 +3603,11 @@ implementation
                methodpointer:=nil;
                parameters:=nil;
              end;
+
+         if assigned(call_self_node) then
+           typecheckpass(call_self_node);
+         if assigned(call_vmt_node) then
+           typecheckpass(call_vmt_node);
 
          finally
            aktcallnode:=oldcallnode;

compiler/nutils.pas
@@ -68,6 +68,7 @@ interface
     procedure checktreenodetypes(n : tnode;typeset : tnodetypeset);
 
     procedure load_procvar_from_calln(var p1:tnode);
+    function get_local_or_para_sym(const aname: string): tsym;
     function maybe_call_procvar(var p1:tnode;tponly:boolean):boolean;
     function load_high_value_node(vs:tparavarsym):tnode;
     function load_self_node:tnode;
@@ -425,7 +426,7 @@ implementation
       end;
 
 
-    function get_local_or_para_sym(const aname:string):tsym;
+    function get_local_or_para_sym(const aname: string): tsym;
       var
         pd : tprocdef;
       begin

tests/webtbs/tw18121.pp
@@ -1,0 +1,17 @@
+{ %recompile }
+
+{$mode objfpc}
+
+{$inline on}
+
+uses
+  uw18121;
+
+var
+  IntO: TPointerList2;
+begin
+  IntO := TPointerList2.Create;
+  IntO.SetF(PInteger(nil));
+  IntO.WriteLn;
+  IntO.Free;
+end.

tests/webtbs/uw18121.pp
@@ -1,0 +1,55 @@
+unit uw18121;
+{$inline on}
+
+interface 
+
+{$mode objfpc}{$H+}
+uses
+  SysUtils;
+
+type
+  { T1 }
+
+  TPointerList = class
+  private
+    i: Pointer;
+    procedure SetF(v: Pointer);
+    function GetF: Pointer;
+  end;
+
+  { TPointerList2 }
+
+  TPointerList2 = class(TPointerList)
+  public
+    procedure SetF(v: PInteger); inline;
+    procedure WriteLn;
+  end;
+
+implementation
+
+  procedure TPointerList.SetF(v: Pointer);
+  begin
+    i := v;
+  end;
+
+  function TPointerList.GetF: Pointer;
+  begin
+    Result := i;
+  end;
+
+  { TPointerList2 }
+
+  procedure TPointerList2.SetF(v: PInteger); inline;
+  begin
+    inherited SetF(Pointer(v));
+  end;
+
+  procedure TPointerList2.WriteLn;
+  var
+    S: string;
+  begin
+    S := Format('%P', [i]);
+    System.WriteLn(S);
+  end;
+
+end.

commit 241d75cdbfb56844215e2c1196b614c18cbc61d7

* wait till the end of typecheckpass before we load a call context's self   parameter instead of immediately doing it in the constructor of the call   node, and then only create it if we actually need it.
  It was previously created in the call node constructor because it needs to be
  done before pass_1 (which is where it is actually used) due to pass_1 possibly
  being performed in the context of inlining (and then a wrong self parameter
  may be found, or none at all), and it was done unconditionally because at that
  point we don't know yet whether or not a self parameter will be necessary (as
  we haven't resolved the overloads/procdef yet).

  The problem with this is that if we use the parentfpstruct way of handling
  accesses to outer scope locals/parameters, we need to know all locals/
  parameters that will be accessed from nested routines after typecheckpass,
  otherwise we get crashes. The problem was that if a call to an RTL routine was
  generated by the compiler in a routine nested inside a method during pass_1,
  and this nested routine itself did not access self of the method (so self was
  not added to its parentfpstruct during the typecheckpass), then the
  unconditional reference to self when creating the call caused a compiler
  crash (introduced in r30908)

git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@31197 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/ncal.pas
@@ -52,7 +52,9 @@ interface
          cnf_objc_processed,     { the procedure name has been set to the appropriate objc_msgSend* variant -> don't process again }
          cnf_objc_id_call,       { the procedure is a member call via id -> any ObjC method of any ObjC type in scope is fair game }
          cnf_unit_specified,     { the unit in which the procedure has to be searched has been specified }
-         cnf_call_never_returns  { information for the dfa that a subroutine never returns }
+         cnf_call_never_returns, { information for the dfa that a subroutine never returns }
+         cnf_call_self_node_done { the call_self_node has been generated if necessary
+                                   (to prevent it from potentially happening again in a wrong context in case of constant propagation or so) }
        );
        tcallnodeflags = set of tcallnodeflag;
 
@@ -69,6 +71,8 @@ interface
           function  gen_procvar_context_tree_self:tnode;
           function  gen_procvar_context_tree_parentfp:tnode;
           function  gen_self_tree:tnode;
+          function  use_caller_self(check_for_callee_self: boolean): boolean;
+          procedure maybe_gen_call_self_node;
           function  gen_vmt_tree:tnode;
           function gen_block_context:tnode;
           procedure gen_hidden_parameters;
@@ -1363,11 +1367,6 @@ implementation
          if assigned(current_structdef) and
             assigned(mp) then
            begin
-             { can't determine now yet if it will be necessary or not, so
-               always create it if there is a 'self' symbol in the current
-               context }
-             if get_local_or_para_sym('self')<>nil then
-               call_self_node:=load_self_node;
             { only needed when calling a destructor from an exception block in a
               contructor of a TP-style object }
             if is_object(current_structdef) and
@@ -2201,6 +2200,67 @@ implementation
               selftree:=methodpointer.getcopy;
           end;
         result:=selftree;
+      end;
+
+    function tcallnode.use_caller_self(check_for_callee_self: boolean): boolean;
+      var
+        i: longint;
+        ps: tparavarsym;
+      begin
+        result:=false;
+        { is there a self parameter? }
+        if check_for_callee_self then
+          begin
+            ps:=nil;
+            for i:=0 to procdefinition.paras.count-1 do
+              begin
+                ps:=tparavarsym(procdefinition.paras[i]);
+                if vo_is_self in ps.varoptions then
+                  break;
+                ps:=nil;
+              end;
+
+            if not assigned(ps) then
+              exit;
+          end;
+
+        { we need to load the'self' parameter of the current routine as the
+          'self' parameter of the called routine if
+            1) we're calling an inherited routine
+            2) we're calling a constructor via type.constructorname and
+               type is not a classrefdef (i.e., we're calling a constructor like
+               a regular method)
+            3) we're calling any regular (non-class/non-static) method via
+               a typenode (the methodpointer is then that typenode, but the
+               passed self node must become the current self node)
+
+          In other cases, we either don't have to pass the 'self' parameter of
+          the current routine to the called one, or methodpointer will already
+          contain it (e.g. because a method was called via "method", in which
+          case the parser already passed 'self' as the method pointer, or via
+          "self.method") }
+        if (cnf_inherited in callnodeflags) or
+           ((procdefinition.proctypeoption=potype_constructor) and
+            not((methodpointer.resultdef.typ=classrefdef) or
+                (cnf_new_call in callnodeflags)) and
+               (methodpointer.nodetype=typen) and
+               (methodpointer.resultdef.typ=objectdef)) or
+           (assigned(methodpointer) and
+            (procdefinition.proctypeoption<>potype_constructor) and
+            not(po_classmethod in procdefinition.procoptions) and
+            not(po_staticmethod in procdefinition.procoptions) and
+            (methodpointer.nodetype=typen)) then
+          result:=true;
+      end;
+
+
+    procedure tcallnode.maybe_gen_call_self_node;
+      begin
+        if cnf_call_self_node_done in callnodeflags then
+          exit;
+        include(callnodeflags,cnf_call_self_node_done);
+        if use_caller_self(true) then
+          call_self_node:=load_self_node;
       end;
 
 
@@ -3673,6 +3733,8 @@ implementation
                methodpointer:=nil;
                parameters:=nil;
              end;
+
+         maybe_gen_call_self_node;
 
          if assigned(call_self_node) then
            typecheckpass(call_self_node);

tests/test/jvm/testall.bat
@@ -314,3 +314,5 @@ ppcjvm -O2 -g -B  -CTinitlocals tsetstring
 if %errorlevel% neq 0 exit /b %errorlevel%
 java -Dfile.encoding=UTF-8 -cp ..\..\..\rtl\units\jvm-java;. tsetstring
 if %errorlevel% neq 0 exit /b %errorlevel%
+ppcjvm -O2 -g -B  -Sa tnestcallpass1
+if %errorlevel% neq 0 exit /b %errorlevel%

tests/test/jvm/testall.sh
@@ -184,3 +184,4 @@ $PPC -O2 -g -B -Sa tprop6a -CTautosetterprefix=Set -CTautogetterprefix=Get
 java -Dfile.encoding=UTF-8 -cp ../../../rtl/units/$RTLDIR:. tprop6a
 $PPC -O2 -g -B -Sa tsetstring
 java -Dfile.encoding=UTF-8 -cp ../../../rtl/units/$RTLDIR:. tsetstring
+$PPC -O2 -g -B -Sa tnestcallpass1

tests/test/jvm/tnestcallpass1.pp
@@ -1,0 +1,31 @@
+{$mode delphi}
+
+program tnestcallpass1;
+
+type
+  tncp1_c = class
+    procedure test;
+  end;
+
+
+procedure tncp1_c.test;
+
+var
+  l: longint;
+
+  function nest(const s: unicodestring): longint;
+    begin
+      l:=5;
+      if length(s)=5 then
+        nest:=l
+      else
+        nest:=3;
+    end;
+
+begin
+  nest('abcdef');
+end;
+
+
+begin
+end.